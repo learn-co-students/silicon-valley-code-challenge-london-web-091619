e4fa74a3f8779c1590c9d7bb2174b3115b5a6a67
Luke Prendergast <luke.prendergast94@gmail.com>
2019-09-22 20:07:49 +0100

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,13 @@
 class FundingRound
+attr_accessor :type,:investment
+attr_reader :startup,:venture_capitalist
+
+def initialize(startup,venture_capitalist,type,investment)
+  @startup = startup
+  @venture_capitalist = venture_capitalist
+  @type = type
+  @investment = investment
+end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,9 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+startup1 = Startup.new("The Big Thingy", "John", "Thingyness")
+venturecapitalist1 = VentureCapitalist.new("John",  1000000)
+fund1 = startup1.sign_contract(venturecapitalist1, "Angel", 2.0)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
